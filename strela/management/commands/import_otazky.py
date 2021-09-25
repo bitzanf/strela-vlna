4ef89b115eaeba9ce9e1640fdf08b02db0a2dd0d
Filip Bitzan <filip.bitzan.25569@gmail.com>
2021-09-25 19:45:31 +0200

    zemna import skriptu

--- strela/management/commands/import_otazky.py
+++ strela/management/commands/import_otazky.py
@@ -6,6 +6,8 @@ from django.db import transaction
 from strela.models import Otazka
 from strela.utils  import ProgressBar
 
+import re
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -27,6 +29,9 @@ class Command(BaseCommand):
         parser.add_argument('-f',
             dest='file',
             help='Importní soubor')    
+        parser.add_argument('-t',
+            dest='typotazky',
+            help='Typ Otázky')     
 
 
     @transaction.atomic
@@ -34,7 +39,8 @@ class Command(BaseCommand):
         dry_run = options['dry_run']
         progressbar = options['progressbar']        
         file = options['file']
-        fileobj=[]
+        typotazky = options['typotazky']
+        fileobj:list[str] = []
         vstup = open(file, "r")
         for radek in vstup.readlines():
             fileobj.append(radek.strip())
@@ -60,19 +66,23 @@ class Command(BaseCommand):
                     reseni += "; {}".format(line)
                 v += 1 
             if line == "":
-                if v >1:
+                if v > 1:
                     vyhodnoceni = 1 
-                else: 
-                    vyhodnoceni = 0  
+                else:
+                    if re.match('^[\\d\\+\\-\\*/,. ]+$', reseni.strip()):
+                        vyhodnoceni = 0
+                        reseni = reseni.strip().replace(' ', '').replace(',', '.')
+                    else:
+                        vyhodnoceni = 1
                 if Otazka.objects.filter(zadani=zadani).exists():
-                    if dry_run:       
-                        print("zadani: {} obtiznost: {} reseni:{} vyhodnoceni: {} " .format(zadani,obtiznost,reseni,vyhodnoceni))    
+                    if dry_run:
+                        print("zadani: {} obtiznost: {} reseni: {} vyhodnoceni: {} " .format(zadani,obtiznost,reseni,vyhodnoceni))
                         print("Otázka je duplicitní !!\n")
                         count = 0
                         reseni = ""
                         v = 0
                 else:    
-                    Otazka.objects.create(typ='X', stav=0, vyhodnoceni=vyhodnoceni, obtiznost=obtiznost, zadani=zadani.strip(), reseni=reseni.strip() )
+                    Otazka.objects.create(typ=typotazky, stav=0, vyhodnoceni=vyhodnoceni, obtiznost=obtiznost, zadani=zadani.strip().replace('___\\n___', '\n'), reseni=reseni.strip().replace('___\\n___', '\n'))
                     count = 0
                     reseni = ""
                     v = 0
@@ -83,10 +93,10 @@ class Command(BaseCommand):
             vyhodnoceni = 0     
         if Otazka.objects.filter(zadani=zadani).exists():
             if dry_run:       
-                print("zadani: {} obtiznost: {} reseni:{} vyhodnoceni: {}" .format(zadani,obtiznost,reseni,vyhodnoceni))    
+                print("zadani: {} obtiznost: {} reseni: {} vyhodnoceni: {}" .format(zadani,obtiznost,reseni,vyhodnoceni))    
                 print("Otázka je duplicitní !!\n")
         else:    
-            Otazka.objects.create(typ='X', stav=0, vyhodnoceni=vyhodnoceni, obtiznost=obtiznost, zadani=zadani.strip(), reseni=reseni.strip() )
+            Otazka.objects.create(typ=typotazky, stav=0, vyhodnoceni=vyhodnoceni, obtiznost=obtiznost, zadani=zadani.strip(), reseni=reseni.strip() )
             imp += 1
         print("Zpracováno {} řádků, naimportováno {} zadání".format(len(fileobj), imp))
 
