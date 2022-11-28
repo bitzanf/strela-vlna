3bac216793d4e919a8a3d312e43c404692774124
Filip Bitzan <filip.bitzan.25569@gmail.com>
2022-11-29 00:12:31 +0100

    hodiny (but ugly af)

--- strela/static/js/clock.js
+++ strela/static/js/clock.js
@@ -2,6 +2,7 @@ var secondHand;
 var minsHand;
 var hourHand;
 var timer;
+var showSeconds = false;
 
 function updateClock() {
     const now = new Date();
@@ -17,17 +18,24 @@ function updateClock() {
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
 
-    var dst = (soutez_start.getTime() + soutez_len * 60000) - now.getTime();
+function updateTimer() {
+    if (soutez_start == null) {
+        timer.innerHTML = 'Soutěž ještě nebyla zahájena';
+        return;
+    }
+
+    var dst = (soutez_start + soutez_len * 60000) - Date.now();
     if (dst >= 0) {
         var dst_h = Math.floor((dst % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var dst_m = Math.floor((dst % (1000 * 60 * 60)) / (1000 * 60));
-        var dst_s = Math.floor((dst % (1000 * 60)) / 1000);
+        if (showSeconds) var dst_s = Math.floor((dst % (1000 * 60)) / 1000);
         
         var outstr = '';
         if (dst_h > 0) outstr += dst_h + 'h ';
         if (dst_m > 0) outstr += dst_m + 'm ';
-        if (dst_s > 0) outstr += dst_s + 's ';
+        if (showSeconds) if (dst_s > 0) outstr += dst_s + 's ';
         timer.innerHTML = outstr;
 
         if (dst < 15 * 60 * 1000) {
@@ -43,9 +51,35 @@ function updateClock() {
 }
 
 function startSoutez() {
-    soutez_start = new Date();
+    soutez_start = Date.now();
     const btn = document.getElementById('startbtn');
     btn.disabled = true;
+
+    showSeconds = document.getElementById('showSeconds').checked;
+
+    let offset = 0;
+    const manualTimeStr = document.getElementById('manualTime').value;
+    if (manualTimeStr == "") return;
+
+    const manualTime = new Number(manualTimeStr);
+    if (manualTime < 0) offset = 0;
+    else if (manualTime > soutez_len) offset = soutez_len;
+    else offset = manualTime;
+
+    soutez_start -= (soutez_len - offset) * 60000;
+}
+
+function toggleShowSeconds(obj) {
+    showSeconds = obj.checked;
+}
+
+function fullscreen() {
+    const target = document.getElementById('fullscreen_target');
+    target.requestFullscreen()
+    .then(function() {
+        timer.style.color = 'white';
+        target.style.cursor = 'none';
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -53,6 +87,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     minsHand = document.querySelector('.min-hand');
     hourHand = document.querySelector('.hour-hand');
     timer = document.querySelector('.timer');
-    setInterval(updateClock, 1000);
-    updateClock();
+
+    const updater = function() {updateClock(); updateTimer();};
+    setInterval(updater, 1000);
+    updater();
+});
+
+document.addEventListener('fullscreenchange', function(event) {
+    const nazev = document.getElementById('nazev_souteze');
+    if (document.fullscreenElement === null) {
+        nazev.style.removeProperty('color');
+        timer.style.removeProperty('color');
+        document.getElementById('fullscreen_target').style.removeProperty('cursor');
+    } else {
+        nazev.style.color = 'white';
+    }
 });
